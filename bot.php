b813e2d397a6acd39201b84d8aa1aa8de1ab68fa
qwerty2528 <qwerty2528@gmail.com>
2016-12-08 17:10:10 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -28,7 +28,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'555' => [$messages],
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
