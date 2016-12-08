3c6a7431c95da4584683fa09da2a284ff5401870
qwerty2528 <qwerty2528@gmail.com>
2016-12-08 15:11:56 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,10 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text,
+				
+				'type' => 'text',
+				'1' => $1,
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
